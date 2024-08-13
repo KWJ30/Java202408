8c23481a7eefcbca7a9ca936c20b0600e52a6f4f
KWJ30 <gidtjrwns30@naver.com>
2024-08-13 15:35:47 +0900

    first commit

---
+++ FirstJava/src/Hello1.java
@@ -0,0 +1,9 @@
+ /*주석*/
+public class Hello1 {
+
+	public static void main(String[] args) {
+		System.out.println("자바는처음...");// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HelloJava/src/sw/mbccom/kr/Hello2.java
@@ -0,0 +1,8 @@
+package sw.mbccom.kr;
+
+public class Hello2 {
+	public static void main(String[] args) {
+		
+	
+	System.out.println("Hello"); }
+}
